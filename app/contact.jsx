b07c8689d2388ec0d14800551d35d4bf9ee093a6
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2025-04-17 09:16:51 +0530

    p-3, file based routing

---
+++ app/contact.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Link} from 'expo-router'
+
+const Contact = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contact Page</Text>
+      <Link href="/" style={styles.link}> Back Home</Link>
+      <Link href="/about" style={styles.link}>About Page</Link>
+    </View>
+  )
+}
+
+export default Contact
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    title: {
+        fontWeight: 'bold',
+        fontsize: 18
+    },
+    link:{
+        marginVertical:10,
+        borderBottomWidth:1
+    }
+})
--- app/index.jsx
+++ app/index.jsx
@@ -1,36 +1,50 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import Logo from '../assets/img/logo_light.png'
+import {Link} from 'expo-router'
+
 
 const Home = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>index, hello, testing expo router</Text>
-      <Text style={{marginTop:10, marginBottom:30}}> Reading List App </Text>
-      <View style={styles.card}>
-        <Text> This is a card </Text>
-      </View>
+    return (
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img} />
+            <Text style={[styles.title, {color:'purple'}]}>index, hello, testing expo router</Text>
+            <Text style={{ marginTop: 10, marginBottom: 30 }}> Reading List App </Text>
+            <View style={styles.card}>
+                <Text> This is a card </Text>
+            </View>
 
-    </View>
-  )
+            <Link href="/about" style={styles.link}>About Page</Link>
+            <Link href="/contact" style={styles.link}>Contact Page</Link>
+
+        </View>
+    )
 }
 
 export default Home
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
 
-    title :{
-      fontWeight:'bold',
-      fontsize:18
+    title: {
+        fontWeight: 'bold',
+        fontsize: 18
     },
-    card:{
-        backgroundColor:'#eee',
-        padding:20,
-        borderRadius:5,
-        boxShadow:'4px 4px rgba(0,0,0,0.1)'
+    card: {
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)'
+    },
+    img: {
+        marginVertical: 20
+    },
+    link:{
+        marginVertical:10,
+        borderBottomWidth:1
     }
 })
