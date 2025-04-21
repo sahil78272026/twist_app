94eb253a39d515c6a8156a755abc39a00f9aa60f
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2025-04-21 21:58:58 +0530

    unpro

--- app/profile.jsx
+++ app/profile.jsx
@@ -7,6 +7,7 @@ const Profile = () => {
     return (
         <View>
             <Text>Profile Page, adding this comment, qki aaj man nahi kar rha </Text>
+            <Text>aaj bhi kuch nahi kar paya, consistency ke liye bas itna likh rha hu </Text>
             <Link href="/about" style={styles.link}>About Page</Link>
             <Link href="/contact" style={styles.link}>Contact Page</Link>
             <Link href="/" style={styles.link}> Back Home</Link>
