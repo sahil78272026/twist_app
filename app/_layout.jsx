ccfceb0cc51c8728712cb09cbf17f7846b617108
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2025-04-22 22:25:14 +0530

    themes, partial

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,23 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, useColorScheme } from 'react-native'
 import React from 'react'
-import {Stack} from 'expo-router'
+import { Stack } from 'expo-router'
+import { Colors } from '../constants/Colors'
+
 
 const RootLayout = () => {
+
+    const colorScheme = useColorScheme() // light or dark
+    console.log(colorScheme)
+    const theme = Colors[colorScheme]??Colors.light // ?? is used if not found any value then providing default
+
+
     return (
         <View style={{ flex: 1 }}>
 
             <Stack screenOptions={{
-                headerStyle:{
-                    backgroundColor:'#ddd'
+                headerStyle: {
+                    backgroundColor: theme.navBackground
                 },
-                headerTintColor:'#333',
+                headerTintColor: theme.title,
 
             }}>
-                <Stack.Screen name='index' options={{title:'Home'}} />
-                <Stack.Screen name='about' options={{title:'About'}} />
-                <Stack.Screen name='contact' options={{title:'Contact', headerShown:false}} />
-                <Stack.Screen name='profile' options={{title:'Profile'}} />
-
+                <Stack.Screen name='index' options={{ title: 'Home' }} />
+                <Stack.Screen name='about' options={{ title: 'About' }} />
+                <Stack.Screen name='contact' options={{ title: 'Contact', headerShown: false }} />
+                <Stack.Screen name='profile' options={{ title: 'Profile' }} />
             </Stack>
 
         </View>
