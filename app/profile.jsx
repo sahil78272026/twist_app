806df5db0c3b3bd0b0af29ab9f1d635a11023e52
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2025-05-19 21:37:29 +0530

    const

--- app/profile.jsx
+++ app/profile.jsx
@@ -26,6 +26,7 @@ const Profile = () => {
             <Text> Consistency</Text>
             <Text>Consistency</Text>
             <Text> Consistency </Text>
+            <Text> Consistemcy</Text>
             <Link href="/about" style={styles.link}>About Page</Link>
             <Link href="/contact" style={styles.link}>Contact Page</Link>
             <Link href="/" style={styles.link}> Back Home</Link>
